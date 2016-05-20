e55745e5b3b50288e8cc02719e4dacd0b8b51896
onlytiancai <onlytiancai@gmail.com>
2016-05-20 23:03:48 +0800

    add -inline, -fields参数

--- README.md
+++ README.md
@@ -12,10 +12,28 @@ DNSPod命令行版, 需要申请login_token，参考：https://support.dnspod.cn
     
     # 设置login_token环境变量
     export DP_LOGIN_TOKEN='10000,e31588236fe82510c7'
+
     # 查看域名列表
     python dpcli.py domain list
-    # 查看mydomain.com记录列表
-    python dpcli.py record list domain=mydomain.com
-    # 给mydomain.com添加一条记录
-    python dpcli.py record create domain=mydomain.com sub_domain=www record_type=A record_line=默认 value=8.8.8.8
 
+    # 查看ihuhao.com记录列表
+    python dpcli.py record list domain=ihuhao.com
+
+    # 给ihuhao.com添加一条记录
+    python dpcli.py record create domain=ihuhao.com sub_domain=www \
+        record_type=A record_line=默认 value=8.8.8.8
+   
+    # 显示ihuhao.com下所有记录，记录属性放在一行内，方便进行grep, awk等过滤 
+    python dpcli.py record list domain=ihuhao.com -inline 
+
+    # 显示ihuhao.com下所有记录，记录属性放在一行内，且只显示记录类型和记录值
+    python dpcli.py record list domain=ihuhao.com -inline -fields=type,value
+
+    #查看ihuhao.com下所有A记录去重后的ip列表
+    python dpcli.py record list domain=ihuhao.com -fields=type,value -inline \
+        | grep "records.type: A" | awk '{print $4}' | sort | uniq
+
+    # 探测ihuhoa.com下所有A记录IP的80端口能否访问通
+    python dpcli.py record list domain=ihuhao.com -fields=type,value -inline \
+        | grep "records.type: A" | awk '{print $4}' | sort | uniq \
+        | xargs -I {} nc -v -w 1 {} -z 80
--- dpcli.py
+++ dpcli.py
@@ -1,25 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-DNSPod命令行版, 需要申请login_token，参考：https://support.dnspod.cn/Kb/showarticle/tsid/227/
-
-帮助
-
-    python dpcli.py --help
-    python dpcli.py domain --help
-    python dpcli.py record --help
-
-示例：
-    
-    # 设置login_token环境变量
-    export DP_LOGIN_TOKEN='10000,e31588236fe82510c7'
-    # 查看域名列表
-    python dpcli.py domain list
-    # 查看mydomain.com记录列表
-    python dpcli.py record list domain=mydomain.com
-    # 给mydomain.com添加一条记录
-    python dpcli.py record create domain=mydomain.com sub_domain=www record_type=A record_line=默认 value=8.8.8.8
-
+DNSPod命令行版
 '''
 import os
 import sys
@@ -53,35 +35,40 @@ def encode(obj):
 
     return obj if english else repr(obj)
 
+def print_fields(key, obj, args):
+    sep = ' ' if args.inline else '\n'
+    keys = obj.keys()
+    if args.fields != 'all' and key != 'status':
+        keys = [k.strip() for k in args.fields.split(',')]
+        keys = [k for k in keys if k in obj.keys()]
+    print sep.join(['%s.%s: %s' % (key, k, encode(obj[k])) for k in sorted(keys)])
 
-def print_result(rsp):
+
+def print_result(rsp, args):
     for key in rsp:
         if isinstance(rsp[key], dict):
             print '=' * 20, key
-            for key2 in rsp[key]:
-                print '%s.%s: %s' % (key, key2, encode(rsp[key][key2]))
+            print_fields(key, rsp[key], args)
         elif isinstance(rsp[key], list):
             for i, obj in enumerate(rsp[key]):
                 print '=' * 20, key, i
-                for key2 in obj:
-                    print '%s.%s: %s' % (key, key2, encode(obj[key2]))
+                print_fields(key, obj, args)
 
 
-def domain_api(*args):
+def domain_api(cmd, subcmd, args):
     try:
-        api = '%s/%s.%s' % (BASE_URI, args[0].capitalize(), args[1].capitalize())
-        data = dict(arg.split('=') for arg in args[2:] if arg.find('=') > 0)
+        api = '%s/%s.%s' % (BASE_URI, cmd.capitalize(), subcmd.capitalize())
+        data = dict(arg.split('=') for arg in args.args if arg.find('=') > 0)
         print api, data
         data.update({"login_token": DP_LOGIN_TOKEN, "format": "json"})
         rsp = urllib2.urlopen(url=api, data=urllib.urlencode(data))
-        print_result(json.loads(rsp.read()))
+        print_result(json.loads(rsp.read()), args)
     except Exception, ex:
         raise
         print ex
 
-
-parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, 
-        description=__doc__)
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description=__doc__)
 sub_group = parser.add_subparsers(title='subcommands')
 for group_name in cmd_groups:
     group = sub_group.add_parser(group_name)
@@ -89,11 +76,10 @@ for group_name in cmd_groups:
     for cmd in cmd_groups[group_name]:
         p = sub_cmd.add_parser(cmd)
         p.add_argument('args', type=str, nargs='*')
+        p.add_argument('-inline', default=False,  action='store_true')
+        p.add_argument('-fields', default='all')
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit()
     args = parser.parse_args()
-    domain_api(*sys.argv[1:])
+    domain_api(sys.argv[1], sys.argv[2], args)
